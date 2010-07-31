331bd8067cce88a87df1c08bd163896637dff6a7
Richard Outten (mbp) <outtenr@gmail.com>
2010-07-31 06:38:26 -0400

    switched to using the embed_html plugin force everything to be base64 encoded (no size checking)

--- lib/rack/embed.rb
+++ lib/rack/embed.rb
@@ -1,14 +1,12 @@
 require 'rack/utils'
 require 'cgi'
+require 'embed_html'
 
 module Rack
   class Embed
     def initialize(app, opts = {})
       @app = app
-      @max_size = opts[:max_size] || 1024
-      @mime_types = opts[:mime_types] || %w(text/css application/xhtml+xml text/html)
-      @threaded = opts[:threaded] || false
-      @timeout = opts[:timeout] || 0.5
+      @mime_types = opts[:mime_types] || %w(application/xhtml+xml text/html)
     end
 
     def call(env)
@@ -19,15 +17,16 @@ module Rack
       if !ua || ua !~ /WebKit|Gecko|Opera|Konqueror|MSIE 8.0/
         return @app.call(env)
       end
-
+      
       original_env = env.clone
       response = @app.call(env)
       return response if !applies_to?(response)
 
       status, header, body = response
-      body = css?(header) ? css_embed_images(body.first, original_env) : html_embed_images(body.first, original_env)
+      body = EmbedHtml::Embeder.new(body.first).process
       header['Content-Length'] = Rack::Utils.bytesize(body).to_s
 
+      
       [status, header, [body]]
     rescue Exception => ex
       env['rack.errors'].write("#{ex.message}\n") if env['rack.errors']
@@ -35,85 +34,6 @@ module Rack
     end
 
     private
-
-    def unescape(url)
-      CGI.unescapeHTML(url)
-    end
-
-    def escape(url)
-      CGI.escapeHTML(url)
-    end
-
-    def css_embed_images(body, env)
-      body.gsub!(/url\(([^\)]+)\)/) do
-        "url(#{escape get_image(env, unescape($1))})"
-      end
-      body
-    end
-
-    def html_embed_images(body, env)
-      body.gsub!(/(<img[^>]+src=)("[^"]+"|'[^']+')/) do
-        img = $1
-        src = unescape($2)
-        "#{img}#{src[0..0]}#{escape(get_image(env, src[1..-2]))}#{src[-1..-1]}"
-      end
-      body
-    end
-
-    def get_image(env, src)
-      return src if src =~ %r{^\w+://|^data:}
-      path = src.dup
-      begin
-        if path[0..0] != '/'
-          base = env['PATH_INFO']
-          i = base.rindex('/')
-          path = base[0..i] + path
-        end
-
-        query = ''
-        i = path.index('?')
-        if i
-          query = path[i+1..-1]
-          path = path[0...i]
-        end
-
-        uri = query && !query.empty? ? "#{path}?#{query}" : path
-
-        inclusion_env = env.merge('PATH_INFO' => path,
-                                  'REQUEST_PATH' => path,
-                                  'REQUEST_URI' => uri,
-                                  'REQUEST_METHOD' => 'GET',
-                                  'QUERY_STRING' => query)
-        inclusion_env.delete('rack.request')
-
-        status, header, body = if @threaded
-                                 app = @app
-                                 result = nil
-                                 thread = Thread.new { result = app.call(inclusion_env) }
-                                 return src if !thread.join(@timeout)
-                                 result
-                               else
-                                 @app.call(inclusion_env)
-                               end
-
-        type = content_type(header)
-
-        return src if status != 200 || !type
-
-        return src if body.respond_to?(:to_path) && ::File.size(body.to_path) > @max_size
-
-        return src if body.respond_to?(:path) && ::File.size(body.path) > @max_size
-
-        body = join_body(body)
-        return src if Rack::Utils.bytesize(body) > @max_size
-
-        body = [body].pack('m')
-        body.gsub!("\n", '')
-        "data:#{type};base64,#{body}"
-      rescue => ex
-        src
-      end
-    end
 
     def applies_to?(response)
       status, header, body = response
@@ -127,19 +47,11 @@ module Rack
       response[2] = [body = join_body(body)]
 
       # Something to embed?
-      if css? header
-        body.include?('url(')
-      else
-        body =~ /<img[^>]+src=/
-      end
+      body =~ /<img[^>]+src=/
     end
 
     def content_type(header)
       header['Content-Type'] && header['Content-Type'].split(';').first.strip
-    end
-
-    def css?(header)
-      content_type(header) == 'text/css'
     end
 
     # Join response body
--- test/test_rack_embed.rb
+++ test/test_rack_embed.rb
@@ -21,36 +21,6 @@ class TestRackEmbed < Test::Unit::TestCase
     assert_same_response(mock_app, esi_app)
   end
 
-  def test_html
-    app = Rack::URLMap.new({
-      '/'      => const([200, {'Content-Type' => 'text/html'}, ['<img src="/image"/>']]),
-      '/image' => const([200, {'Content-Type' => 'image/png'}, ['image_data']])
-    })
-
-    esi_app = Rack::Embed.new(app)
-    assert_equal ['<img src="data:image/png;base64,aW1hZ2VfZGF0YQ=="/>'], esi_app.call('SCRIPT_NAME' => '', 'PATH_INFO' => '/', 'HTTP_USER_AGENT' => 'WebKit')[2]
-  end
-
-  def test_css
-    app = Rack::URLMap.new({
-      '/'      => const([200, {'Content-Type' => 'text/css'}, ['background: url(/image)']]),
-      '/image' => const([200, {'Content-Type' => 'image/png'}, ['image_data']])
-    })
-
-    esi_app = Rack::Embed.new(app)
-    assert_equal ['background: url(data:image/png;base64,aW1hZ2VfZGF0YQ==)'], esi_app.call('SCRIPT_NAME' => '', 'PATH_INFO' => '/', 'HTTP_USER_AGENT' => 'WebKit')[2]
-  end
-
-  def test_threaded
-    app = Rack::URLMap.new({
-      '/'      => const([200, {'Content-Type' => 'text/css'}, ['background: url(/image)']]),
-      '/image' => const([200, {'Content-Type' => 'image/png'}, ['image_data']])
-    })
-
-    esi_app = Rack::Embed.new(app, :threaded => true)
-    assert_equal ['background: url(data:image/png;base64,aW1hZ2VfZGF0YQ==)'], esi_app.call('SCRIPT_NAME' => '', 'PATH_INFO' => '/', 'HTTP_USER_AGENT' => 'WebKit')[2]
-  end
-
   def test_threaded_timeout
     app = Rack::URLMap.new({
       '/'      => const([200, {'Content-Type' => 'text/css'}, ['background: url(/image)']]),
@@ -69,17 +39,6 @@ class TestRackEmbed < Test::Unit::TestCase
 
     esi_app = Rack::Embed.new(app)
     assert_equal ['background: url(/image)'], esi_app.call('SCRIPT_NAME' => '', 'PATH_INFO' => '/', 'HTTP_USER_AGENT' => 'WebKit')[2]
-  end
-
-  def test_file
-    app = Rack::URLMap.new({
-      '/'      => const([200, {'Content-Type' => 'text/css'}, ['background: url(/image)']]),
-      '/image' => const([200, {'Content-Type' => 'image/png'}, File.open('test/test.image')])
-    })
-
-    esi_app = Rack::Embed.new(app)
-    assert_equal ['background: url(data:image/png;base64,VGhpcyBpcyBhIHRlc3QgaW1hZ2U=)'],
-    esi_app.call('SCRIPT_NAME' => '', 'PATH_INFO' => '/', 'HTTP_USER_AGENT' => 'WebKit')[2]
   end
 
   def test_invalid_browser
